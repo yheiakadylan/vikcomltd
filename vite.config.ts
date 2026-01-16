e8f7abe804f7dc865565c30cc3ba888450294a54
yheiakadylan <haitrinh2605204@gmail.com>
2026-01-16 10:15:50 +0700

    delete PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,56 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'Vikcom Dashboard',
-        short_name: 'Vikcom',
-        description: 'Order Management System',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
-      workbox: {
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/wsrv\.nl\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'images-thumbnails',
-              expiration: {
-                maxEntries: 1000,
-                maxAgeSeconds: 60 * 60 * 24 * 60, // 60 Days
-                purgeOnQuotaError: true
-              },
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          },
-          {
-            urlPattern: /^https:\/\/.*(firebasestorage\.googleapis\.com).*/i,
-            handler: 'NetworkOnly',
-          }
-        ]
-      }
-    })
   ],
 })
